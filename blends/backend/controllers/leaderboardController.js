f5eb979475758b8af9f1bde7f5100ab563303854
Adrian7305 <adrianronan7305@gmail.com>
2025-11-05 15:47:55 +0530

    Revert "Merge pull request #1 from BOSS-009/feature/event-team-qr"
    This reverts commit d48dbccde095f1b34c623344c673b2c311e2d8ae, reversing
    changes made to eb191855d1ea6cdd8debb98645a3841c626318ef.

--- blends/backend/controllers/leaderboardController.js
+++
@@ -1,8 +0,0 @@
-const Team = require("../models/team");
-
-exports.getLeaderboard = async (req, res, next) => {
-  try {
-    const teams = await Team.find().sort({ score: -1 }).populate("event leader members", "title name email");
-    res.json(teams);
-  } catch (err) { next(err); }
-};
--- blends/backend/routes/attendance.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const qrController = require("../controllers/qrController");
-const auth = require("../middleware/auth");
-
-// POST /api/attendance/checkin
-router.post("/checkin", auth, qrController.checkin);
-
-module.exports = router;
